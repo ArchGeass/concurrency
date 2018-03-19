f07140a368b181661f760348e8f70f9cc0dd951f
ArchGeass <642640169@qq.com>
2018-03-19 18:09:40 +0800

    并发test

---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,18 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description:用来标记线程"不安全"的类或者写法
+ * Create by SunChenLong
+ * 2018/3/19,16:42
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
